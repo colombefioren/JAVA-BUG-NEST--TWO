083d709663e7393a06b994cc458190093ea237db
Tokimahery <101268219+tokyramarozaka@users.noreply.github.com>
2025-08-05 11:47:08 +0300

    feat: add Department class

--- src/employee_management/Department.java
+++ src/employee_management/Department.java
@@ -4,12 +4,19 @@ package employee_management;
  * TODO : a department is a collection of employees. It has a name and a set of employees in it.
  */
 public class Department {
-    /**
-     * TODO : Employees should never ever be duplicated in a single department.
-     * Please check if it already exists in the department before inserting if need be. Duplicate
-     * here means : their employee id and name are the same.
-     *
-     * TODO : for our HR officer, give a function to show all details about employees, including
-     * their pay.
-     */
+    private String name;
+    private Set<Employee> employees;
+
+    public Department(String name, Set<Employee> employees) {
+        this.name = name;
+        this.employees = employees;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Employee> getEmployees () {
+        return employees;
+    }
 }
