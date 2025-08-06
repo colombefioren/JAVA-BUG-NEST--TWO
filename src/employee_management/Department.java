be6f7b87861529766dd126546dfdeb9e18bf1cc1
colombefioren <colomberakotonjanahary@gmail.com>
2025-08-06 18:23:41 +0300

    refactor: remove comments and added final to Department class attributes

--- src/employee_management/Department.java
+++ src/employee_management/Department.java
@@ -2,12 +2,9 @@ package employee_management;
 
 import java.util.Set;
 
-/**
- * TODO : a department is a collection of employees. It has a name and a set of employees in it.
- */
 public class Department {
-    private String name;
-    private Set<Employee> employees;
+    private final String name;
+    private final Set<Employee> employees;
 
     public Department(String name, Set<Employee> employees) {
         this.name = name;
