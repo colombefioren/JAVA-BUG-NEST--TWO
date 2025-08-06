4670e48c6cb5620da5af74f4e8587c7dd10fc2e0
colombefioren <colomberakotonjanahary@gmail.com>
2025-08-06 18:15:47 +0300

    fix: made classes have private attributes and added set and get methods

--- src/employee_management/FullTimeEmployee.java
+++ src/employee_management/FullTimeEmployee.java
@@ -1,13 +1,37 @@
 package employee_management;
 
 public class FullTimeEmployee {
-    public int employeeId;
-    public String name;
-    public double monthlySalary;
+    private int employeeId;
+    private String name;
+    private double monthlySalary;
 
     public FullTimeEmployee(int employeeId, String name, double monthlySalary) {
         this.employeeId = employeeId;
         this.name = name;
+        this.monthlySalary = monthlySalary;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMonthlySalary(double monthlySalary) {
         this.monthlySalary = monthlySalary;
     }
 
--- src/employee_management/PartTimeEmployee.java
+++ src/employee_management/PartTimeEmployee.java
@@ -1,16 +1,48 @@
 package employee_management;
 
 public class PartTimeEmployee {
-    public int employeeId;
-    public String name;
-    public double hoursWorked;
-    public double hourlyRate;
+    private int employeeId;
+    private String name;
+    private double hoursWorked;
+    private double hourlyRate;
 
 
     public PartTimeEmployee(int employeeId, String name, double hoursWorked, double hourlyRate) {
         this.employeeId = employeeId;
         this.name = name;
         this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
         this.hourlyRate = hourlyRate;
     }
 
